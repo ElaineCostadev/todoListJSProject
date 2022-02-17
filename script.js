d8319d07961b9a295deadcf1dd35de768be0c2a9
ElaineCosta <elainerscosta@hotmail.com>
2022-02-17 17:02:17 -0300

    tentando o requesito 5

--- script.js
+++ script.js
@@ -1,13 +1,61 @@
+/* 
+
+
+// texto da tarefa a ser digitada
+function listagemDasTarefas(){
+  //fazer a listar e imprimir na pagina
+  let listagemTarefas = document.querySelector("#lista-tarefas");
+  let criarElement = document.createElement("li");
+  listagemTarefas.appendChild(criarElement);
+  criarElement.appendChild(document.createTextNode(listagemTarefas.value));
+  listagemTarefas.value = "";
+  
+  };
+
+  
+//botao ok - registra o que foi escrito
+function adcionaTarefa(){
+let button = document.querySelector("#criar-tarefa")
+button.addEventListener("click", function(){
+  let inputText = document.querySelector("#texto-tarefa");
+  let novaText = inputText.value;
+
+
+})
+}
+
+button.addEventListener("click",listagemDasTarefas); */
+
+
+ 
 
 
 
-let inputarValor = document.getElementById("texto-tarefa");
-inputarValor.addEventListener("input", function(event){
-  let newinput = document.querySelector("#texto-tarefa");
-    newinput.innerText = event.target.value;    
-    console.log("Cliquei");
-});
+// texto da tarefa a ser digitada
+function listagemDasTarefas(){
+  //fazer a listar e imprimir na pagina
+  
+  console.log(listagemTarefas);
+  };
 
+  
+//botao ok - registra o que foi escrito
+function adcionaTarefa(){
+
+  let button = document.querySelector("#criar-tarefa")
+  button.addEventListener("click", function(){
+  let inputText = document.querySelector("#texto-tarefa");
+  let novaText = inputText.value;
+
+  let listagemTarefas = document.querySelector("#lista-tarefas");
+  let criarElement = document.createElement("li");
+  listagemTarefas.appendChild(criarElement);
+  let listagem = document.createTextNode(listagemTarefas.value);
+  criarElement.appendChild(listagem);
+  listagemTarefas.value = novaText;
+
+})
+}
 
 
 
