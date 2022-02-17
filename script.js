7b3df450879e3b54bc82c0524d0779edf05b2d3a
ElaineCosta <elainerscosta@hotmail.com>
2022-02-17 19:38:49 -0300

    ajustando o 5 e 6 - indo para o 7

--- script.js
+++ script.js
@@ -1,61 +1,54 @@
-/* 
-
-
-// texto da tarefa a ser digitada
-function listagemDasTarefas(){
-  //fazer a listar e imprimir na pagina
-  let listagemTarefas = document.querySelector("#lista-tarefas");
-  let criarElement = document.createElement("li");
-  listagemTarefas.appendChild(criarElement);
-  criarElement.appendChild(document.createTextNode(listagemTarefas.value));
-  listagemTarefas.value = "";
   
-  };
 
-  
-//botao ok - registra o que foi escrito
-function adcionaTarefa(){
-let button = document.querySelector("#criar-tarefa")
-button.addEventListener("click", function(){
-  let inputText = document.querySelector("#texto-tarefa");
-  let novaText = inputText.value;
+/* function adcionaTarefa(){ */
 
-
-})
-}
-
-button.addEventListener("click",listagemDasTarefas); */
-
-
- 
-
-
-
-// texto da tarefa a ser digitada
-function listagemDasTarefas(){
-  //fazer a listar e imprimir na pagina
-  
-  console.log(listagemTarefas);
-  };
-
-  
-//botao ok - registra o que foi escrito
-function adcionaTarefa(){
-
+  //botao ok - registra o que foi escrito
   let button = document.querySelector("#criar-tarefa")
   button.addEventListener("click", function(){
+    //recebe o texto digitado no input
   let inputText = document.querySelector("#texto-tarefa");
-  let novaText = inputText.value;
+  inputText.value;
 
+
+// O que foi escrito no input vai para a Lista OL de lista-tarefas criando uma nova li.
   let listagemTarefas = document.querySelector("#lista-tarefas");
   let criarElement = document.createElement("li");
   listagemTarefas.appendChild(criarElement);
-  let listagem = document.createTextNode(listagemTarefas.value);
-  criarElement.appendChild(listagem);
-  listagemTarefas.value = novaText;
+  //recebe como filho e cria uma nova lista com o CreateTextNode para criar o nó de texto
+  http://devfuria.com.br/javascript/dom-create-element/
+  criarElement.appendChild(document.createTextNode(inputText.value));
 
-})
-}
+  //limpar input do texto
+  //https://cursos.alura.com.br/forum/topico-limpando-a-caixa-de-texto-20395
+  inputText.value = "";
+  
+
+//botao que ao clicar fica cinza
+  criarElement.addEventListener("click", function(){
+    criarElement.style.backgroundColor = "rgb(128, 128, 128)";
+//se for clicado 1x, pintar de cinza
+//ao clicar 2x, pintar o selecionado e deixar o anterior em branco
+
+
+
+
+    if(criarElement.value === "rgb(128, 128, 128)"){
+        console.log("mantem");
+    }else{
+      
+    }
+
+
+  })
+
+  
+
+});
+
+/*  adcionaTarefa();
+} */
+
+//ao clicar na lista de tarefas criada, ficará cinza
 
 
 
