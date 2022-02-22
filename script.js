e53f78925c39c86ac73d975f89b46e54b5aa00c1
ElaineCosta <elainerscosta@hotmail.com>
2022-02-22 11:25:22 -0300

    finalizando requesito 8

--- script.js
+++ script.js
@@ -17,104 +17,40 @@
   //recebe como filho e cria uma nova lista com o CreateTextNode para criar o nó de texto
   http://devfuria.com.br/javascript/dom-create-element/
   criarElement.appendChild(document.createTextNode(inputText.value));
-
   //limpar input do texto
   //https://cursos.alura.com.br/forum/topico-limpando-a-caixa-de-texto-20395
   inputText.value = "";
 
 
 
-/* adcionaTarefa();
-};
+  let elementSelected = document.createElement("selected");
+/*   criarElement.getElementsByClassName("selected")[0];
  */
-
-
-//botao que ao clicar fica cinza
-//function selecionarTirarCinza()
-/* function clicarNoCinza (){  */ 
-
- 
- 
-/* }
-clicarNoCinza(); */
-
-//fonte https://developer.mozilla.org/pt-BR/docs/Web/API/EventTarget/addEventListener
-
-
-/* function clicarBranco(){ */
-
-  criarElement.addEventListener("click", function(){
-    let gray = criarElement.style.backgroundColor = "gray";
-
-  if(criarElement >=2){
-    return criarElement.style.backgroundColor = "white";
-  }
-});
- 
-
-  /*   let whiteColor = criarElement.style.backgroundColor = "white"; */
-
-  /*   if(grayColor === 1 || grayColor > 3){
-      console.log(grayColor);
-      return grayColor;
-        }if (grayColor >= 2 || grayColor >=4) {
-          console.log(whiteColor);
-          return whiteColor;
-        }
-      }); */
-
-  /*   criarElement.addEventListener("change", function(){
-    let whiteColor = criarElement.style.backgroundColor = "white";
-    if(whiteColor > 1){
-      console.log(criarElement);
-      return whiteColor;
-    }
-  */
-
+/*   criarElement.appendChild(elementSelected); */
   
+
+  criarElement.addEventListener("click", function(event){
+   let classSelected = document.querySelector(".selected");
+   
+   if(classSelected !== null ){
+    classSelected.classList.remove("selected");
+   }
+  
+   event.target.classList.add("selected");
+
+
+
+
+
+ 
 
 });
 
 
-/* clicarBranco() */
-
-/* if(clicarNoCinza() ===1){
-  return clicarNoCinza();
-}else{
-  clicarBranco();
-}
 
 
 
 
+});
 
 
- 
- /*  let whiteColor = criarElement.style.backgroundColor = "white"; */
-  
-/*   
-  if (criarElement.inputMode < 1){
-    console.log(grayColor);
-    return grayColor;
-    
-  }else if (criarElement.inputMode > 1) {
-    console.log(whiteColor);
-   return whiteColor;
-  
-   */
-
-  
-  
-
-    
-
-
-
-
-/* }
-clicarNoCinza (); */
-
-
-/* 
-let clickElement = document.getElementById("li");
-clickElement.classList.toggle(hide); */
